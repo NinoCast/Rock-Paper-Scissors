4027a03dfaaa0b30a748877eb7a6a69a74a3877f
Nino Castellanos <ninocastellanos@gmail.com>
2023-03-09 12:20:40 -0800

    created function that randomly selects a choice

---
+++ script.js
@@ -0,0 +1,15 @@
+const options = ["rock", "paper", "scissors"];
+
+
+function getComputerChoice() {
+const choice = options[Math.floor(Math.random() * options.length)];
+console.log(choice);
+
+}
+
+function getPlayerChoice(options) {
+    return getPlayerChoice + options
+
+    
+}
+getComputerChoice();
